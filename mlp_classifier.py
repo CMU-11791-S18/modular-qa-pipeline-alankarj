b948d9bbf8ffbdb3bb80e969690141c0594d6e54
alankar <alankarj@andrew.cmu.edu>
2018-09-19 10:12:03 -0400

    added mlp and svm classifiers

--- mlp_classifier.py
+++ mlp_classifier.py
@@ -1,12 +1,12 @@
 from Classifier import Classifier
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.neural_network import MLPClassifier
 
 
 # This is a subclass that extends the abstract class Classifier.
-class MultinomialNaiveBayes(Classifier):
+class MLP(Classifier):
 
     # The abstract method from the base class is implemented here
     # to return multinomial naive bayes classifier
     def buildClassifier(self, X_features, Y_train):
-        clf = MultinomialNB().fit(X_features, Y_train)
+        clf = MLPClassifier().fit(X_features, Y_train)
         return clf
--- svm_classifier.py
+++ svm_classifier.py
@@ -1,12 +1,12 @@
 from Classifier import Classifier
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 
 
 # This is a subclass that extends the abstract class Classifier.
-class MultinomialNaiveBayes(Classifier):
+class SupportVectorMachine(Classifier):
 
     # The abstract method from the base class is implemented here
     # to return multinomial naive bayes classifier
     def buildClassifier(self, X_features, Y_train):
-        clf = MultinomialNB().fit(X_features, Y_train)
+        clf = SVC().fit(X_features, Y_train)
         return clf
