b14f36e62f2a35293a626d430dafe97e0759b5e4
alankar <alankarj@andrew.cmu.edu>
2018-09-26 12:11:38 -0400

    added linearsvc

--- svm_classifier.py
+++ svm_classifier.py
@@ -1,5 +1,5 @@
 from Classifier import Classifier
-from sklearn.svm import SVC
+from sklearn.svm import SVC, LinearSVC
 
 
 # This is a subclass that extends the abstract class Classifier.
@@ -8,5 +8,5 @@ class SupportVectorMachine(Classifier):
     # The abstract method from the base class is implemented here
     # to return multinomial naive bayes classifier
     def buildClassifier(self, X_features, Y_train):
-        clf = SVC().fit(X_features, Y_train)
+        clf = LinearSVC().fit(X_features, Y_train)
         return clf
