057846ec2679ba9c7512662aba2d1de6ce1a6025
alankar <alankarj@andrew.cmu.edu>
2018-09-26 13:44:16 -0400

    changed settings for mlp

--- mlp_classifier.py
+++ mlp_classifier.py
@@ -8,5 +8,5 @@ class MLP(Classifier):
     # The abstract method from the base class is implemented here
     # to return multinomial naive bayes classifier
     def buildClassifier(self, X_features, Y_train):
-        clf = MLPClassifier().fit(X_features, Y_train)
+        clf = MLPClassifier(random_state=123, batch_size=256, tol=1e-3).fit(X_features, Y_train)
         return clf
